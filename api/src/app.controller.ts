f1afeb0756ef0bff549ee78b9f07e42ab25bff7e
Callie Stoscup <calliestoscup@gmail.com>
2024-01-02 20:37:12 -0500

    Started working on photo upload feature

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.appService.uploadFile(file);
+  }
 }
