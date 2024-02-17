530cd1f16ea41fa9b0133df23e1003f860df3cd5
Callie Stoscup <calliestoscup@gmail.com>
2024-02-16 21:39:58 -0500

    Styled guest speaker component

--- frontend/src/Pages/GuestSpeakers.tsx
+++ frontend/src/Pages/GuestSpeakers.tsx
@@ -2,7 +2,6 @@ import {
   Box,
   useMediaQuery,
   Image,
-  Text,
   Accordion,
   AccordionButton,
   AccordionIcon,
@@ -10,106 +9,151 @@ import {
   AccordionPanel,
 } from "@chakra-ui/react";
 import BodyHeading from "../Components/BodyHeading";
-import BodyText from "../Components/BodyText";
 import Section from "../Components/Section";
+import BodyText from "../Components/BodyText";
+import MyButton from "../Components/MyButton";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import Paragraph from "../Components/Paragraph";
 const michaelduran = require("../../src/images/michaelduran.jpeg");
 
-const homeText = [
-  "Hey there! I'm the self-taught coding wizard behind the screens at USAA, where I sprinkle my magic on the Frontend using the classic trifecta: HTML, CSS, and JavaScript. By day, I'm weaving through code, and by night, I shift gears to become a dedicated husband and a superhero dad to an energetic 5-year-old who's likely to code circles around me in the future.",
-  "My journey into the tech world is backed by a Bachelor's in Media Communications, and I'm currently leveling up with an Associate's in Computer Science. While my daily grind is all about the front-end, I don’t shy away from diving into other areas. I'm well-versed in React.js for that extra spice in front-end development and have a knack for Java, because who says you can't have the best of both worlds?",
-  "Outside of my tech endeavors, you'll find me living the best of both worlds – whether I'm lost in the latest gaming adventure, mastering the art of Muay Thai, or taking refreshing walks to recharge.",
-  "My life is a unique tapestry of code, gaming kicks, and precious family moments, proving that blending passion with profession and personal life is not just possible but incredibly rewarding.",
-  "Let's connect and share our journeys!",
+type Speaker = {
+  name: string;
+  date: string;
+  sessionText: string[];
+  bioText: string[];
+  websiteUrl: string;
+  photoUrl: string;
+};
+
+const data: Speaker[] = [
+  {
+    name: "Michael Duran",
+    date: "February 29, 2024 8PM EST",
+    sessionText: [
+      "Join me as I break down the cool ways we use HTML, CSS, and JavaScript to make sure everyone can get around our digital world at USAA, no matter what. We're all about knocking down barriers and building up a web that's open for all.",
+      "I'll share some behind-the-scenes tricks and tales of how these tech tools help us roll out the welcome mat online. Get ready for a casual chat on making tech accessible and why it matters to everyone in our community.",
+      "See you there!",
+    ],
+    bioText: [
+      "Hey there! I'm the self-taught coding wizard behind the screens at USAA, where I sprinkle my magic on the Frontend using the classic trifecta: HTML, CSS, and JavaScript. By day, I'm weaving through code, and by night, I shift gears to become a dedicated husband and a superhero dad to an energetic 5-year-old who's likely to code circles around me in the future.",
+      "My journey into the tech world is backed by a Bachelor's in Media Communications, and I'm currently leveling up with an Associate's in Computer Science. While my daily grind is all about the front-end, I don’t shy away from diving into other areas. I'm well-versed in React.js for that extra spice in front-end development and have a knack for Java, because who says you can't have the best of both worlds?",
+      "Outside of my tech endeavors, you'll find me living the best of both worlds – whether I'm lost in the latest gaming adventure, mastering the art of Muay Thai, or taking refreshing walks to recharge.",
+      "My life is a unique tapestry of code, gaming kicks, and precious family moments, proving that blending passion with profession and personal life is not just possible but incredibly rewarding.",
+      "Let's connect and share our journeys!",
+    ],
+    websiteUrl: "https://www.linkedin.com/in/michael-duran-b5b002203/",
+    photoUrl: "https://coding-with-callie.s3.amazonaws.com/michaelduran.jpeg",
+  },
 ];
 
 const GuestSpeakers = () => {
+  const [speakers, setSpeakers] = useState(data);
+
   const [isLargerThan500] = useMediaQuery("(min-width: 500px)");
   const [isLargerThan700] = useMediaQuery("(min-width: 700px)");
   const [isLargerThan900] = useMediaQuery("(min-width: 900px)");
 
   return (
     <Box m={"0 auto"}>
-      <Section
-        screenSizeParameter={isLargerThan700}
-        alignItemsCenter={true}
-        direction={isLargerThan900 ? "row" : "column"}
-      >
-        <BodyHeading textAlignCenter={false}>Michael Duran</BodyHeading>
-      </Section>
+      {speakers.map((speaker) => {
+        console.log("SPEAKER", speaker);
+        return (
+          <Section
+            screenSizeParameter={isLargerThan700}
+            alignItemsCenter={false}
+            gapSize={10}
+            direction={isLargerThan900 ? "row" : "column"}
+          >
+            <Image
+              src={michaelduran}
+              borderRadius="50%"
+              h={isLargerThan500 ? "250px" : "280px"}
+              boxShadow="lg"
+            />
+            <Box w="60%" display="flex" flexDirection="column" gap={6}>
+              <BodyHeading textAlignCenter={false} removeMargin>
+                {speaker.name}
+              </BodyHeading>
+              <Accordion defaultIndex={[0]} allowToggle borderColor="black">
+                <AccordionItem borderColor={"#45446A"}>
+                  <h2>
+                    <AccordionButton>
+                      <Box
+                        as="span"
+                        flex="1"
+                        textAlign="left"
+                        color={"#45446A"}
+                        fontWeight="900"
+                      >
+                        Meet-up Date
+                      </Box>
+                      <AccordionIcon color={"#45446A"} />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    <Paragraph>{speaker.date}</Paragraph>
+                  </AccordionPanel>
+                </AccordionItem>
 
-      <Section
-        screenSizeParameter={isLargerThan700}
-        alignItemsCenter={true}
-        gapSize={10}
-        direction={isLargerThan900 ? "row" : "column"}
-      >
-        <Image
-          src={michaelduran}
-          borderRadius="50%"
-          h={isLargerThan500 ? "250px" : "280px"}
-          boxShadow="lg"
-        />
-        <Box w="60%">
-          <Box display="flex" w="100%">
-            <Text w="30%">Meet-up Date:</Text>
-            <Text>February 29, 2024</Text>
-          </Box>
-          <Accordion defaultIndex={[0]} allowMultiple borderColor="black">
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box as="span" flex="1" textAlign="left">
-                    Session Description
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}></AccordionPanel>
-            </AccordionItem>
+                <AccordionItem borderColor={"#45446A"}>
+                  <h2>
+                    <AccordionButton>
+                      <Box
+                        as="span"
+                        flex="1"
+                        textAlign="left"
+                        color={"#45446A"}
+                        fontWeight="900"
+                      >
+                        Session Description
+                      </Box>
+                      <AccordionIcon color={"#45446A"} />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    <BodyText
+                      textBlocks={speaker.sessionText}
+                      textAlignCenter={false}
+                    />
+                  </AccordionPanel>
+                </AccordionItem>
 
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box as="span" flex="1" textAlign="left">
-                    About Michael
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                {homeText.map((item) => {
-                  return <Text mb={3}>{item}</Text>;
-                })}
-              </AccordionPanel>
-            </AccordionItem>
-          </Accordion>
-        </Box>
-      </Section>
-      <Box>
-        <BodyText textBlocks={homeText} textAlignCenter={false} />
-      </Box>
+                <AccordionItem borderColor={"#45446A"}>
+                  <h2>
+                    <AccordionButton>
+                      <Box
+                        as="span"
+                        flex="1"
+                        textAlign="left"
+                        color={"#45446A"}
+                        fontWeight="900"
+                      >
+                        About Michael
+                      </Box>
+                      <AccordionIcon color={"#45446A"} />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    <BodyText
+                      textBlocks={speaker.sessionText}
+                      textAlignCenter={false}
+                    />
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+              <Link
+                to="https://www.linkedin.com/in/michael-duran-b5b002203/"
+                target="_blank"
+              >
+                <MyButton widthSize="100%">Contact Michael</MyButton>
+              </Link>
+            </Box>
+          </Section>
+        );
+      })}
     </Box>
   );
 };
 
 export default GuestSpeakers;
-
-{
-  /* <Box display="flex" border="1px" justifyContent="space-between">
-<Paragraph bold>Meet-up Date:</Paragraph>
-<Paragraph>February 29, 2024</Paragraph>
-</Box>
-<Box display="flex" border="1px" justifyContent="space-between">
-<Paragraph bold>Topic:</Paragraph>
-<Paragraph>
-  Join me as I break down the cool ways we use HTML, CSS, and
-  JavaScript to make sure everyone can get around our digital world
-  at USAA, no matter what. We're all about knocking down barriers
-  and building up a web that's open for all. I'll share some
-  behind-the-scenes tricks and tales of how these tech tools help us
-  roll out the welcome mat online. Get ready for a casual chat on
-  making tech accessible and why it matters to everyone in our
-  community. See you there!
-</Paragraph>
-</Box> */
-}
