f565d82164b382b3dd141ab98a1d73bba83e6e94
Callie Stoscup <calliestoscup@gmail.com>
2024-03-08 15:10:47 -0500

    Worked more on workshops page

--- frontend/src/Pages/Workshops.tsx
+++ frontend/src/Pages/Workshops.tsx
@@ -40,10 +40,14 @@ const fullstack = require("../images/fullstack.png");
 //   },
 // ];
 
-const details = [
-  "When I was learning to code, I spent a maximum of a week or two working on a single project. When I started my software engineering position, I realized how much my projects were lacking when it comes to: error handling, testing, logging, security, project management, documentation, etc.",
-  "So, I thought I'd start a self-paced workshop where we build a relatively simple application, but spend the time making sure it is actually usable.",
-  "Whenever I learn a new technology, I create a Todo List with it to make sure I understand the fundamentals. So, this is a great workshop to choose if you are new to NestJS!",
+const why = [
+  "When I was learning to code, I spent a maximum of a week or two working on a single project. I made all the quick portfolio projects: weather app, movie list, online clothing store.",
+  "When I started my software engineering position, however, I realized how much my projects were lacking when it comes to: error handling, testing, logging, security, project management, documentation, etc. So, I decided to start recording myself building fullstack applications from planning to deployment, spending the necessary time to make sure users could actually use them.",
+];
+
+const mantra = [
+  "You'll see me put this mantra into practice while completing my workshops. A lot of engineers get stuck trying to architect elegant code or make a feature absolutely perfect before making their pull request and it ends up being a detriment to the project. On the flip side, a lot of bootcamps move so quickly, students end up coming up with 80% of a solution they don't have the time to fully understand before moving on and never touching the code again. In my workshops, I try to find the right balance.",
+  "My general rule of thumb is to go with the easiest solution to code first. I don't worry about issues until they start being issues. As soon as my code in one area starts to be a problem for another area, then I refactor. I find that this workflow helps me understand in practice a lot of the concepts we're taught in school/bootcamps.",
 ];
 
 export type Workshop = {
@@ -61,12 +65,25 @@ const Workshops = () => {
   const workshops = useLoaderData() as Workshop[];
 
   return (
-    <Section screenSizeParameter={false} alignItemsCenter={false}>
-      <Heading fontSize={28} mb={6} color="#79A9CD" textAlign="center">
-        Workshops
-      </Heading>
-      <BodyText textBlocks={details} textAlignCenter={false} />
-      <Box display="flex" flexDirection="column" gap={10}>
+    <>
+      <Section screenSizeParameter={false} alignItemsCenter={false}>
+        <Heading fontSize={28} mb={6} color="#79A9CD" textAlign="center">
+          Coding with Callie Workshops
+        </Heading>
+      </Section>
+      <Section screenSizeParameter={false} alignItemsCenter={false}>
+        <Heading fontSize={28} mb={6} color="#79A9CD" w="100%">
+          My Workshop Origin Story
+        </Heading>
+        <BodyText textBlocks={why} textAlignCenter={false} />
+      </Section>
+      <Section screenSizeParameter={false} alignItemsCenter={false}>
+        <Heading fontSize={28} mb={6} color="#79A9CD" w="100%">
+          Make it work first, and then make it better
+        </Heading>
+        <BodyText textBlocks={mantra} textAlignCenter={false} />
+      </Section>
+      <Box display="flex" flexDirection="column" gap={10} w="60%">
         {workshops.map((workshop) => {
           return (
             <Box
@@ -101,13 +118,7 @@ const Workshops = () => {
           );
         })}
       </Box>
-      {/* <BodyText textBlocks={todoListDetails} textAlignCenter={true} /> */}
-      {/* <Box display="flex" gap={10}>
-        <Link to="todo-list">
-          <MyButton>Todo List Workshop</MyButton>
-        </Link>
-      </Box> */}
-    </Section>
+    </>
   );
 };
 
