df044a5179e862aae12caa3dca1870386db101a0
Callie Stoscup <calliestoscup@gmail.com>
2024-03-08 15:25:42 -0500

    Feeling pretty good about workshops page

--- frontend/src/Pages/Workshops.tsx
+++ frontend/src/Pages/Workshops.tsx
@@ -5,41 +5,6 @@ import Section from "../Components/Section";
 import BodyText from "../Components/BodyText";
 const fullstack = require("../images/fullstack.png");
 
-// const workshops = [
-//   {
-//     name: "Project Planning Tool: Fullstack",
-//     description:
-//       "Build a fullstack project planning tool that you can subsequently use to plan all of your portfolio projects with React, ChakraUI, NestJS, and PostgreSQL.",
-//     available: true,
-//     image: fullstack,
-//     path: "todo-list",
-//   },
-//   {
-//     name: "Project Planning Tool: Deployment",
-//     description:
-//       "Deploy your Project Planning Tool with AWS, Kubernetes, Docker, Helm, Terraform, and more!",
-//     available: false,
-//     image: fullstack,
-//     path: "todo-list",
-//   },
-//   {
-//     name: "Project Planning Tool: Frontend",
-//     description:
-//       "Build the frontend of the project planning tool (using Callie's deployed backend) that you can subsequently use to plan all of your portfolio projects with React and ChakraUI.",
-//     available: false,
-//     image: fullstack,
-//     path: "todo-list",
-//   },
-//   {
-//     name: "Project Planning Tool: Backend",
-//     description:
-//       "Build the backend of the project planning tool (using Callie's deployed frontend) that you can subsequently use to plan all of your portfolio projects with NestJS and PostgreSQL.",
-//     available: false,
-//     image: fullstack,
-//     path: "todo-list",
-//   },
-// ];
-
 const why = [
   "When I was learning to code, I spent a maximum of a week or two working on a single project. I made all the quick portfolio projects: weather app, movie list, online clothing store.",
   "When I started my software engineering position, however, I realized how much my projects were lacking when it comes to: error handling, testing, logging, security, project management, documentation, etc. So, I decided to start recording myself building fullstack applications from planning to deployment, spending the necessary time to make sure users could actually use them.",
@@ -83,41 +48,56 @@ const Workshops = () => {
         </Heading>
         <BodyText textBlocks={mantra} textAlignCenter={false} />
       </Section>
-      <Box display="flex" flexDirection="column" gap={10} w="60%">
-        {workshops.map((workshop) => {
-          return (
-            <Box
-              backgroundColor="white"
-              borderRadius={4}
-              p={10}
-              boxShadow="lg"
-              flex={1}
-              display="flex"
-              flexDirection="column"
-              gap={10}
-            >
-              <Heading fontSize={28} color="#79A9CD" textAlign="center">
-                {workshop.name}
-              </Heading>
-
-              <Text color="#45446A">{workshop.description}</Text>
-
-              <Image
-                src={fullstack || workshop.photo}
-                boxShadow="lg"
+      <Section screenSizeParameter={false} alignItemsCenter={false}>
+        <Heading fontSize={28} mb={6} color="#79A9CD" w="100%">
+          Let's get to work 💪🏻
+        </Heading>
+        <Box
+          display="flex"
+          gap={10}
+          flexWrap="wrap"
+          justifyContent="center"
+          maxW="900px"
+        >
+          {workshops.map((workshop) => {
+            return (
+              <Box
+                backgroundColor="white"
                 borderRadius={4}
-                objectFit="cover"
-              />
+                p={10}
+                boxShadow="lg"
+                flex={1}
+                display="flex"
+                flexDirection="column"
+                gap={10}
+                minW="400px"
+                maxW="400px"
+              >
+                <Heading fontSize={28} color="#79A9CD" textAlign="center">
+                  {workshop.name}
+                </Heading>
 
-              <Box m="0 auto">
-                <Link to={`/workshops/${workshop.id}`}>
-                  <MyButton>Learn More</MyButton>
-                </Link>
+                <Text color="#45446A" flex={1}>
+                  {workshop.description}
+                </Text>
+
+                <Image
+                  src={fullstack || workshop.photo}
+                  boxShadow="lg"
+                  borderRadius={4}
+                  objectFit="cover"
+                />
+
+                <Box m="0 auto">
+                  <Link to={`/workshops/${workshop.id}`}>
+                    <MyButton>Learn More</MyButton>
+                  </Link>
+                </Box>
               </Box>
-            </Box>
-          );
-        })}
-      </Box>
+            );
+          })}
+        </Box>
+      </Section>
     </>
   );
 };
