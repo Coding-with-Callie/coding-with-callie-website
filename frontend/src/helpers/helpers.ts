cec7e3ce5cd08c55f0b67764ff0cd68d3413d89f
Callie Stoscup <calliestoscup@gmail.com>
2024-08-11 18:48:47 -0400

    Removed unusred functions

--- frontend/src/helpers/helpers.ts
+++ frontend/src/helpers/helpers.ts
@@ -1,7 +1,3 @@
-import axios from "axios";
-import { Workshop } from "../Pages/Workshops";
-import { showNotification } from "..";
-
 export const isInvalidName = (name: string) => {
   if (name === "") return true;
   return false;
@@ -20,44 +16,4 @@ export const isInvalidEmail = (email: string) => {
 export const isInvalidComments = (comments: string) => {
   if (comments === "") return true;
   return false;
-};
-
-export const transferCart = async (
-  cart: Workshop[],
-  navigate: (path: string) => void,
-  updateUser: (newUser: any) => void,
-  onClose?: () => void
-) => {
-  const token = localStorage.getItem("token");
-
-  for (let i = 0; i < cart.length; i++) {
-    const response: any = await axios
-      .post(
-        `${
-          process.env.REACT_APP_API || "http://localhost:3001/api"
-        }/auth/add-workshop-to-cart`,
-        {
-          workshopId: cart[i].id,
-        },
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      )
-      .catch((error) => {
-        showNotification(error.response.data.message, "error");
-      });
-
-    updateUser(response.data);
-
-    localStorage.setItem(
-      "temp-cart",
-      JSON.stringify(response.data.cart.workshops)
-    );
-
-    if (response.data.cart.workshops.length === 0) {
-      showNotification("You already have access to this workshop", "error");
-      navigate("/my-workshops");
-      if (onClose) onClose();
-    }
-  }
 };
