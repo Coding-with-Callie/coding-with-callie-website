6de18543abaa4f2d79e817b765ae1c9be739c759
Callie Stoscup <calliestoscup@gmail.com>
2024-11-05 16:50:39 -0500

    Bleh

--- api/babel.config.js
+++ api/babel.config.js
@@ -2,5 +2,7 @@ module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-typescript'],
   plugins: [
     ['@babel/plugin-syntax-decorators', { decoratorsBeforeExport: true }],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
 };
