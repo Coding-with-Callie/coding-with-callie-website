e958d64aee928964cb3ee34550b5f35ae103c282
Callie Stoscup <calliestoscup@gmail.com>
2024-11-05 16:56:01 -0500

    pls

--- api/babel.config.js
+++ api/babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-typescript'],
   plugins: [
-    ['@babel/plugin-syntax-decorators', { decoratorsBeforeExport: true }],
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     '@babel/plugin-transform-class-properties',
   ],
