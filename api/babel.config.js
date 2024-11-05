41983265071223663f8e9743aff3285e2e9de2a3
Callie Stoscup <calliestoscup@gmail.com>
2024-11-05 17:28:04 -0500

    add type to one erroring column

--- api/babel.config.js
+++ api/babel.config.js
@@ -3,5 +3,6 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     '@babel/plugin-transform-class-properties',
+    'babel-plugin-transform-typescript-metadata',
   ],
 };
