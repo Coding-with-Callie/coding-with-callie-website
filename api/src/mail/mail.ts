ca8d1f89a470dff4ec3437722cd7f76c92ebf4e4
Callie Stoscup <calliestoscup@gmail.com>
2024-11-09 14:12:35 -0500

    Try with OAuth2

--- api/src/mail/mail.ts
+++ api/src/mail/mail.ts
@@ -1,16 +1,32 @@
 import * as nodemailer from 'nodemailer';
+import { google } from 'googleapis';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const dotenv = require('dotenv');
 dotenv.config();
 
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  'https://developers.google.com/oauthplayground',
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN,
+});
+
+const accessToken = oauth2Client.getAccessToken();
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
   auth: {
-    user: 'calliestoscup',
-    pass: process.env.GMAIL_PASSWORD,
+    type: 'OAuth2',
+    user: 'calliestoscup@gmail.com',
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+    accessToken,
   },
 });
 
