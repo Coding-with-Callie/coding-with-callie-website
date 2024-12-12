1ce6abb1903bddc7b0f1656e43271eb217dc65d0
Callie Stoscup <calliestoscup@gmail.com>
2024-12-12 17:49:50 -0500

    Customize file upload input

--- frontend/src/Components/Forms/FileInput.tsx
+++ frontend/src/Components/Forms/FileInput.tsx
@@ -1,4 +1,6 @@
-import { Box, FormLabel, Input } from "@chakra-ui/react";
+import { Box, FormLabel, Input, Text } from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import MyButton from "../MyButton";
 
 type Props = {
   onChange: React.ChangeEventHandler<HTMLInputElement>;
@@ -7,20 +9,45 @@ type Props = {
 };
 
 const FileInput = ({ onChange, key, label }: Props) => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [fileName, setFileName] = useState<string | null>("No file chosen");
+
+  const handleIconClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    } else {
+    }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setFileName(event.target.files[0].name);
+    } else {
+      setFileName(null);
+    }
+    onChange(event);
+  };
+
   return (
     <Box>
       <FormLabel>{label}</FormLabel>
-      <Input
-        p={0}
-        border="none"
-        borderRadius="0px"
-        type="file"
-        accept="image/*"
-        onChange={onChange}
-        color="#45446A"
-        key={key}
-        id="image"
-      />
+      <Text>{fileName}</Text>
+      <MyButton onClick={handleIconClick}>
+        Upload Image
+        <Input
+          p={0}
+          border="none"
+          borderRadius="0px"
+          type="file"
+          accept="image/*"
+          onChange={handleFileChange}
+          color="#45446A"
+          key={key}
+          id="image"
+          display="none"
+          ref={fileInputRef}
+        />
+      </MyButton>
     </Box>
   );
 };
