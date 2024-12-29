e418bc79676337c15eacc6b1cba2f8c71fba95dc
Callie Stoscup <calliestoscup@gmail.com>
2024-12-29 16:56:06 -0500

    Remove other unnecessary migrations

--- api/src/migrations/1730663020971-drop-feedback-table.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class DropFeedbackTable1730663020971 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "feedback" CASCADE`);
-  }
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
