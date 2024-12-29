06309c8511c6f2cdcc6874ebfa6c6cfb73727641
Callie Stoscup <calliestoscup@gmail.com>
2024-12-29 16:51:41 -0500

    Remove cart migration

--- api/src/migrations/1730662843343-drop-cart.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class DropCart1730662843343 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "cart" CASCADE`);
-  }
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
