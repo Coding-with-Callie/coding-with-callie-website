b9f86df6ec48d116427e62e2a752fc490e62b059
Callie Stoscup <calliestoscup@gmail.com>
2025-01-04 11:18:15 -0500

    Refactored to make a section heading component so section component is more readable

---
+++ frontend/src/Components/Home/SectionHeading.tsx
@@ -0,0 +1,54 @@
+import { Box, useMediaQuery } from "@chakra-ui/react";
+import BodyHeading from "../BodyHeading";
+import EditIcons from "./EditIcons";
+import { useOutletContext } from "react-router-dom";
+import { Context } from "../../App";
+
+type Props = {
+  heading: string;
+  id: number;
+  order: number;
+  numSections: number;
+  edit: boolean;
+  setEdit: React.Dispatch<React.SetStateAction<boolean>>;
+  setPageData: React.Dispatch<React.SetStateAction<any[]>>;
+};
+
+const SectionHeading = ({
+  heading,
+  id,
+  order,
+  numSections,
+  edit,
+  setEdit,
+  setPageData,
+}: Props) => {
+  const { user } = useOutletContext() as Context;
+  const [isLargerThan800] = useMediaQuery("(min-width: 800px)");
+
+  return (
+    <>
+      {!edit && (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          flexDirection={isLargerThan800 ? "row" : "column"}
+        >
+          <BodyHeading>{heading}</BodyHeading>
+          {user.role === "admin" && (
+            <EditIcons
+              id={id}
+              order={order}
+              numSections={numSections}
+              setPageData={setPageData}
+              setEdit={setEdit}
+            />
+          )}
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default SectionHeading;
--- frontend/src/Components/Section.tsx
+++ frontend/src/Components/Section.tsx
@@ -1,11 +1,7 @@
 import { Box, useMediaQuery } from "@chakra-ui/react";
-import BodyHeading from "./BodyHeading";
-import { useOutletContext } from "react-router-dom";
-import { Context } from "../App";
-import TextWithImageAndButton from "./Home/TextWithImageAndButton";
-import EditIcons from "./Home/EditIcons";
 import { useState } from "react";
-import EditableTextWithImageAndButton from "./Home/EditableTextWithImageAndButton";
+import Resource from "./Home/Resource";
+import SectionHeading from "./Home/SectionHeading";
 
 type Props = {
   data: any;
@@ -20,12 +16,8 @@ const Section = ({
   numSections,
   setPageData,
 }: Props) => {
-  const { user } = useOutletContext() as Context;
-
   const [edit, setEdit] = useState(false);
-
   const [isLargerThan900] = useMediaQuery("(min-width: 900px)");
-  const [isLargerThan800] = useMediaQuery("(min-width: 800px)");
 
   return (
     <Box
@@ -38,34 +30,22 @@ const Section = ({
       mx={isLargerThan900 ? "auto" : 8}
       boxShadow="lg"
     >
-      {!edit && (
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="space-between"
-          flexDirection={isLargerThan800 ? "row" : "column"}
-        >
-          <BodyHeading>{data.data.heading}</BodyHeading>
-          {user.role === "admin" && (
-            <EditIcons
-              id={data.id}
-              order={data.data.order}
-              numSections={numSections}
-              setPageData={setPageData}
-              setEdit={setEdit}
-            />
-          )}
-        </Box>
-      )}
-      {data.type === "resource" && edit ? (
-        <EditableTextWithImageAndButton
-          id={data.id}
-          initialState={data.data}
+      <SectionHeading
+        heading={data.data.heading}
+        id={data.id}
+        order={0}
+        numSections={numSections}
+        edit={edit}
+        setEdit={setEdit}
+        setPageData={setPageData}
+      />
+      {data.type === "resource" && (
+        <Resource
+          resource={data.data}
           setPageData={setPageData}
+          edit={edit}
           setEdit={setEdit}
         />
-      ) : (
-        <TextWithImageAndButton data={data.data} />
       )}
     </Box>
   );
